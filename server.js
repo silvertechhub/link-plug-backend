96355eb0e08647036d30c9c5ef9c7282bfff0419
silvertechhub <agubatasylvester@gmail.com>
2022-12-30 00:44:20 +0100

    cors bugs fixed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded());
 
-app.use(cors({origin: ["http://localhost:3000"]}));
+app.use(cors({origin: ["http://localhost:3000", "https://linkplug-api.onrender.com"]}));
 
 app.use('/api/routes', appRoutes);
 app.use('/api/userRoutes', userRoutes);
