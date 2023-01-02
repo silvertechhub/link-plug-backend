557e05e88ab90136e90900803901d29050915e68
silvertechhub <agubatasylvester@gmail.com>
2023-01-02 10:45:46 +0100

    static file error

--- server.js
+++ server.js
@@ -25,15 +25,7 @@ app.use('/api/routes', appRoutes);
 app.use('/api/userRoutes', userRoutes);
    
  
-//  serve static file if in production
-if(process.env.NODE_ENV === 'production'){ 
 
-    app.use(express.static( "./client/build"))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
-    })
-}
 
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
